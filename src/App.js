13418fd122891f2b43fae43897c38b416d866bc4
Chris Athanas <chris@crowdskout.com>
2021-01-27 22:46:33 -0600

    Add modal pop-up code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import './App.css';
+
+// https://github.com/reactjs/react-modal
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   return (
     <div className="App">
-      hello world
+      <button onClick={() => setModalIsOpen(true)}> Open Modal</button>
+      <Modal 
+          isOpen={modalIsOpen} 
+          shouldCloseOnOverlayClick={true}
+          onRequestClose={()=> setModalIsOpen(false)}
+        >
+        <h2>Modal Title</h2>
+        <p>Modal Body</p>
+        <div>
+          <button onClick={() => setModalIsOpen(false)}>Close</button>
+        </div>
+      </Modal>
     </div>
   );
 }
